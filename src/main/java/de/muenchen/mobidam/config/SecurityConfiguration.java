7383f7135639976d2804b4f416340e5a4b238d34
Mirna Rodic <mirna.rodic@muenchen.de>
2024-04-15 13:34:45 +0200

    :bug: Fixed security

--- src/main/java/de/muenchen/mobidam/config/SecurityConfiguration.java
+++ src/main/java/de/muenchen/mobidam/config/SecurityConfiguration.java
@@ -30,6 +30,18 @@ public class SecurityConfiguration {
     public SecurityFilterChain securityFilterChain(final HttpSecurity http) throws Exception {
 
         return http
+                .authorizeHttpRequests((requests) -> requests.requestMatchers("/**",
+                                // allow access to /actuator/info
+                                "/actuator/info",
+                                // allow access to /actuator/health for OpenShift Health Check
+                                "/actuator/health",
+                                // allow access to /actuator/health/liveness for OpenShift Liveness Check
+                                "/actuator/health/liveness",
+                                // allow access to /actuator/health/readiness for OpenShift Readiness Check
+                                "/actuator/health/readiness",
+                                // allow access to /actuator/metrics for Prometheus monitoring in OpenShift
+                                "/actuator/metrics")
+                        .permitAll())
                 .authorizeHttpRequests((authorize) -> authorize
                         .requestMatchers(new AntPathRequestMatcher("/**")).authenticated())
                 .oauth2ResourceServer(oauth2 -> oauth2
